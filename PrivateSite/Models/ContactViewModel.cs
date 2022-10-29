5242dc035aa25ff3a271183b9182218ff3c4e4fa
ZachBarkoviak <zjbarkoviak@outlook.com>
2022-10-29 12:08:46 -0500

    mvp progress

---
+++ PrivateSite/Models/ContactViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations; //Grants access to annotations for validation
+
+namespace PrivateSite.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "*Name is required")] //Makes the field required
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "*Email is required")]
+        [DataType(DataType.EmailAddress)] //Certain formatting is expected (@ symbol, .com, ect...)
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "*Subject is required")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "*Message is required")]
+        [DataType(DataType.MultilineText)] //Makes the textbox for this field bigger
+        public string Message { get; set; }
+    }
+}
+
